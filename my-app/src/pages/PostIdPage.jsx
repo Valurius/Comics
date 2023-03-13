8328799eae8e77aa48cc00b3df216a634571bac5
Valurius <garimar525@gmail.com>
2023-03-13 14:17:48 +0300

    init

---
+++ my-app/src/pages/PostIdPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/PostId.css';
+
+const PostIdPage = () => {
+    return(
+    <div className='App'>
+        <div className='comic'>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/0.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/1.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/2.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/3.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/4.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/5.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/6.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/7.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/8.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/9.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/10.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/11.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/12.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/13.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/14.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/15.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/16.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/17.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/18.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/19.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/20.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/21.jpg'></img>
+        <img className='comicImg' src='../comic/Amazing Spider-Man 1/22.jpg'></img>
+        </div>
+    </div>
+    )
+};
+
+export default PostIdPage;
